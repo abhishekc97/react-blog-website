a72b956e49e9a3af14b80bfc799f60a4fbb9e124
Abhishek Choudhari <abhishekc97@gmail.com>
2022-03-28 19:34:14 -0400

    Add files via upload

---
+++ src/Create.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            // history.go(-1);
+            history.push('/');
+        })
+    }
+    
+    return ( 
+        <div className="create">
+            <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}> 
+                <label>Blog Title: </label>
+                <input
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body: </label>
+                <textarea 
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author: </label>
+                <select 
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button> Add blog</button>
+            </form>
+        </div>
+    );
+}
+ 
+export default Create;
